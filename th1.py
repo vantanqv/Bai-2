2f03d44e9d1943620d0625e4d4d26cc64670cca1
vantanqv <49965682+vantanqv@users.noreply.github.com>
2019-05-16 16:57:47 +0700

    Add files via upload

---
+++ th1.py
@@ -0,0 +1,6 @@
+print("chieu cao tam giac la: ")
+a=int(input())
+print("canh day tuong ung là: ")
+b=int(input())
+S=float(a*b/2)
+print("dien tich tam giac là: "+str(S))
---
+++ th2.py
@@ -0,0 +1,7 @@
+#sửa lỗi của đoạn chương trình
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+    print("ni is big")
+else:
+    print("n2 is big")
